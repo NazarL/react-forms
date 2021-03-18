f563d8b5ffddeee132d2900253847b30552b6b07
NazarL <nazar.lebedenko@gmail.com>
2021-03-18 16:46:40 -0400

    handle clicks with onSubmit

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,20 +2,30 @@ import React, { useState } from "react";
 
 function App() {
   const [name, setName] = useState("");
+  const [headingText, setHeadingText] = useState("");
 
   function handleChange(event) {
     setName(event.target.value);
   }
 
+  function handleClick(event) {
+    setHeadingText(name);
+    //prevents form from default refresh
+    event.preventDefault();
+  }
+
   return (
     <div className="container">
-      <h1>Hello {name}</h1>
-      <input
-        onChange={handleChange}
-        type="text"
-        placeholder="What's your name?"
-      />
-      <button>Submit</button>
+      <h1>Hello {headingText}</h1>
+      <form onSubmit={handleClick}>
+        <input
+          onChange={handleChange}
+          type="text"
+          placeholder="What's your name?"
+          value={name}
+        />
+        <button type="submit">Submit</button>
+      </form>
     </div>
   );
 }
